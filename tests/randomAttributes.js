4a1e9c5c3185b8f77d974eedb7cd01f348ce06dc
AaronOngTW <aaronongtw@gmail.com>
2016-10-12 12:19:02 +0800

    added colours

--- tests/randomAttributes.js
+++ tests/randomAttributes.js
@@ -1,11 +1,15 @@
 eyeType = ['grid','leaf','box']
 
-darkColours = []
+darkColours = ['#144914','#5C1919','#5C3719','#0F3737','#4E521C','#2F1437','#54391C','#132C34','#54451C','#191A3A','#4A1929','#304B19']
 
-lightColours = []
+lightColours = ['#CCECCC','#FFDDDD','#FFECDD','#BCD9D9','#FBFDDB','#D8C2DF','#FFEEDD','#BFD4DB' ,'#FFF6DD','#C8C9E2','#F4D4DE','#E4F5D4']
 
-skribbleDarkColours = []
+skribbleMain = '#0E6660'
 
-skribbleLightColours = []
+skribbleEye = '#B90065'
+
+skribbleDarkColours = ['#0E6660', '#B90065', '#301B73', '#871252', '#A75516', '#25009D']
+
+skribbleLightColours = ['#5ED2CA','#8970DB','#E968AE','#FFAF72']
 
 drawingType = ['straight', 'random']
