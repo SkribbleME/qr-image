5046364f09e63f7b9d93e49c05b966a169e4a82e
sarkunanv <sarkunan.v@skribble.me>
2016-10-12 11:40:10 +0800

    Customize qr options

--- tests/test.js
+++ tests/test.js
@@ -8,9 +8,12 @@ function file(name) {
 var qr = require('./../');
 var text = 'I \u2764\uFE0F QR code!';
 text = 'http://q0.my/9nfy2f98s34+__f';
+var url = '//q0.my/aB2_r2'
 
 var ec_level = 'Q';
 
-qr.image(text, { type: 'svg', ec_level: ec_level, size: 15, parse_url: true }).pipe(file('qrcode.svg'));
-var svgstring = qr.imageSync(text, { type: 'svg', ec_level: ec_level, size: 15, parse_url: true});
-fs.writeFile(__dirname + '/qrcode2.svg', svgstring, function(err){ if(err) console.log(err)});
+// options for svg generation
+var options = { type: 'svg', ec_level: ec_level, size: 10 }
+var qrOptions = { eyeType: 'leaf', eyeColor: 'green', eyeBorderColor: 'purple'}
+var svgstring = qr.imageSync(url, options, qrOptions, 0, 0);
+fs.writeFile(__dirname + '/qrcodetest.svg', svgstring, function(err){ if(err) console.log(err)});
